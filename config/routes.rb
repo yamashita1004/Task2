5104c166e4e1e143ae10e8ddfbdf89e53b8f9420
yamashita1004 <119408342+yamashita1004@users.noreply.github.com>
2022-12-10 14:42:52 +0000

    [finish]1-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  post 'books' => 'books#create'
+  get 'books/show'
+  get 'books/edit'
+  get "/" => "home#top"
+  get 'books/:id' => 'books#index', as: 'book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
