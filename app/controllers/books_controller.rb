262aca53376796f06e882446ac0861801626f5b6
yamashita1004 <119408342+yamashita1004@users.noreply.github.com>
2022-12-13 12:43:57 +0000

    [start]1^5 git commit -m[start]1^5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,20 @@
 class BooksController < ApplicationController
-   def new
-    @book = Book.new
-  end
-  
+def new
+end
   def index
     @books = Book.all
+    @book = Book.new
   end
 
- def create
+  def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to "/books/#{@book.id}}"
+    if @book.save
+      redirect_to book_path(@book.id)
+    flash[:notice] = "Book was successfully created."
+    else
+    @books = Book.all
+    render :index
+    end
   end
 
   def show
@@ -20,13 +24,14 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully created."
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -35,7 +40,8 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title,:body)
+   params.require(:book).permit(:title, :body)
   end
 
 end
+
